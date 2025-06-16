b58c2999681474bafcaadd5365896e9fb14f2ed7
leonardo-programa <leo2008santos@hotmail.com>
2025-06-16 13:55:54 -0300

    Update Etapa 3.c

--- Projeto incremental/Etapa 3.c
+++ Projeto incremental/Etapa 3.c
@@ -2,8 +2,6 @@
 #include <string.h>
 #define Max_Contas 100
 
-int total_Contas = 0;
-int id = 1001;
 
 typedef struct{
     int numero;
@@ -13,7 +11,7 @@ typedef struct{
 
 int mostrarMenu (){
     int opcao;
-      printf ("===== SISTEMA BANCARIO =====\n");
+      printf ("\n===== SISTEMA BANCARIO =====\n");
         printf ("0 - Sair\n");
         printf ("1 - Criar Conta\n");
         printf ("2 - Exibir Contas\n");
@@ -24,117 +22,145 @@ int mostrarMenu (){
         
         printf ("Escolha uma opcao: ");
         scanf ("%d", &opcao);
-
+        printf ("\n===========================\n");
         return opcao;
     }
+    
+int verificaConta(Conta contas[], int numero, int total_Contas){
+    
+    if (total_Contas == 0){
+        printf ("\nNao existem contas criadas!\n");
+        return -1;
+    }
+        for (int i = 0; i < total_Contas; i++){
+            if (numero == contas[i].numero){
+                return i;
+            }
+        }
+    printf ("\nConta nao encontrada!\n");
+    return -2;
+}
 
-void criarConta(Conta contas[]){
-    if (total_Contas >= Max_Contas){
+void criarConta(Conta contas[], int *total_Contas, int *id){
+    if (*total_Contas >= Max_Contas){
         printf ("Limite de contas atingido.");
         return;
     }
-    int i = total_Contas;
-    printf ("\n---CADASTRO---\n");
+    int i = *total_Contas;
+    printf ("\n===== CADASTRO =====\n");
     getchar();
     printf ("Digite o nome do titular: ");
     fgets(contas[i].nome, Max_Contas, stdin);
+    printf ("\n====================\n");
     contas[i].nome[strcspn(contas[i].nome, "\n")] = '\0';
-    contas[i].numero = id;
+    contas[i].numero = *id;
     contas[i].saldo = 0;
 
-    total_Contas++;
-    id++;
+    (*total_Contas)++;
+    (*id)++;
     
-    printf ("\n\n---CONTA---\n");
+    printf ("\n===CONTA===\n");
     printf ("Nome: %s\n", contas[i].nome);
     printf ("Numero: %d\n", contas[i].numero);
-    printf ("Saldo: %.2f\n\n", contas[i].saldo);
+    printf ("Saldo: %.2f\n", contas[i].saldo);
+    printf ("===========\n");
 }
-void exibirConta(Conta contas[]){
+
+void exibirConta(Conta contas[], int total_Contas){
     if (total_Contas == 0){
         printf ("\n\nNao existem contas criadas!\n\n");
     }
     else {
         for (int i = 0; i < total_Contas; i++){
-            printf ("\n---CONTA---\n");
+            printf ("\n===CONTA===\n");
             printf ("Nome: %s\n", contas[i].nome);
             printf ("Numero: %d\n", contas[i].numero);
-            printf ("Saldo: %.2f\n\n", contas[i].saldo);
+            printf ("Saldo: %.2f\n", contas[i].saldo);
+            printf ("===========\n");
         }
     }
 }
-void depositar (Conta contas[]){
+
+void depositar (Conta contas[], int total_Contas){
     int numero;
     float valorDepositado;
+
     printf ("\nDigite o numero da conta: ");
     scanf ("%d", &numero);
 
-    for (int i = 0; i < total_Contas; i++){
-        if (numero == contas[i].numero){
-            printf ("Digite o valor do deposito: ");
-            scanf ("%f", &valorDepositado);
+    printf ("\nBuscando conta...\n");
 
-            if (valorDepositado <= 0){
-                printf ("Valor de deposito invalido.\n\n");
+    int i = verificaConta (contas, numero, total_Contas);
+
+    if (i >= 0){
+        printf ("\nDigite o valor do deposito: ");
+        scanf ("%f", &valorDepositado);
+
+
+        if (valorDepositado <= 0){
+            printf ("Valor de deposito invalido.\n");
             }
-            else {
-                contas[i].saldo += valorDepositado;
-                printf ("---DADOS---\n");
-                printf ("Numero: %d\n", contas[i].numero);
-                printf ("Nome: %s\n", contas[i].nome);
-                printf ("Saldo: %.2f\n\n", contas[i].saldo);
+        else {
+            contas[i].saldo += valorDepositado;
+            printf ("\n===DADOS===\n");
+            printf ("Numero: %d\n", contas[i].numero);
+            printf ("Nome: %s\n", contas[i].nome);
+            printf ("Saldo: %.2f\n", contas[i].saldo);
+            printf ("===========\n");
             }
         }
     }
-}
-void sacar (Conta contas[]){
+
+void sacar (Conta contas[], int total_Contas){
     int numero;
     float valorSacado;
     printf ("\nDigite o numero da conta: ");
     scanf ("%d", &numero);
 
-    for (int i = 0; i < total_Contas; i++){
-        if (numero == contas[i].numero){
-            printf ("Digite o valor a ser sacado: ");
-            scanf ("%f", &valorSacado);
+    printf ("\nBuscando conta...\n\n");
 
-            if (valorSacado <= 0){
-                printf ("Valor de saque invalido.\n\n");
-            }
-            else if (valorSacado > contas[i].saldo){
-                printf ("Valor insuficiente para saque.\n\n");
-            }
-            else {
-                contas[i].saldo -= valorSacado;
+    int i = verificaConta(contas, numero, total_Contas);
 
-            printf ("---DADOS---\n");
-            printf ("Numero: %d\n", contas[i].numero);
-            printf ("Nome: %s\n", contas[i].nome);
-            printf ("Saldo: %.2f\n\n", contas[i].saldo);
-            }
+    if (i >= 0){
+        printf ("Digite o valor a ser sacado: ");
+        scanf ("%f", &valorSacado);
+
+        if (valorSacado <= 0){
+            printf ("Valor de saque invalido.\n\n");
+        }
+        else if (valorSacado > contas[i].saldo){
+            printf ("Valor insuficiente para saque.\n\n");
+        }
+        else {
+            contas[i].saldo -= valorSacado;
+
+        printf ("\n===DADOS===\n");
+        printf ("Numero: %d\n", contas[i].numero);
+        printf ("Nome: %s\n", contas[i].nome);
+        printf ("Saldo: %.2f\n", contas[i].saldo);
+        printf ("===========\n");
         }
     }
 }
-void buscarConta (Conta contas[]){
+
+void buscarConta (Conta contas[], int total_Contas){
     int numero;
-    printf ("Digite o numero da conta: ");
+    printf ("\nDigite o numero da conta: ");
     scanf ("%d", &numero);
 
-    for (int i = 0; i < total_Contas; i++){
-        if (numero == contas[i].numero){
-            printf ("---DADOS---\n");
-            printf ("Numero: %d\n", contas[i].numero);
-            printf ("Nome: %s\n", contas[i].nome);
-            printf ("Saldo: %.2f\n\n", contas[i].saldo);
-        }
+    int i = verificaConta (contas, numero, total_Contas);
+    if (i >= 0){
+        printf ("\n===DADOS===\n");
+        printf ("Numero: %d\n", contas[i].numero);
+        printf ("Nome: %s\n", contas[i].nome);
+        printf ("Saldo: %.2f\n", contas[i].saldo);
+        printf ("===========\n\n");
     }
 }
 
-    
-    
 int main(){
     Conta contas[Max_Contas];
-    int opcao;
+    int opcao, total_Contas, id = 1001;
     do{ 
         opcao = mostrarMenu();
         
@@ -146,26 +172,25 @@ int main(){
             switch (opcao){
 
             case 1:
-                criarConta (contas);
+                criarConta (contas, &total_Contas, &id);
                 break;
             case 2:
-                exibirConta (contas);
+                exibirConta (contas, total_Contas);
                 break;
             case 3:
-                depositar (contas);
+                depositar (contas, total_Contas);
                 break;
             case 4:
-                sacar (contas);
+                sacar (contas, total_Contas);
                 break;
             case 5:
-                buscarConta (contas);
+                buscarConta (contas, total_Contas);
                 break;
             default:
                 printf("Opção Inválida!\n");
                 break;
         }
       }
-
     } while (opcao != 0);
     return 0;
 }
