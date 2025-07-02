078217f13b567f703fa7f8da504ea7976b1acea8
leonardo-programa <leo2008santos@hotmail.com>
2025-07-02 10:07:53 -0300

    Update ex - 3.c

--- Aula 14/ex - 3.c
+++ Aula 14/ex - 3.c
@@ -8,11 +8,32 @@ int main(){
         return 1;
     }
 
-    char nome[30], gmail[30]; int idade;
-    printf ("Digite seu nome: ");
-    fgets(nome, sizeof(nome), arquivo);
-    
+    char nome[50], gmail[100]; int idade;
 
-    fprintf(arquivo, "Nome: %s, Idade: %d, Gmail: %[^/n]", nome, idade, gmail);
+    printf ("Digite seu nome: ");
+
+    scanf("%29[^\n]", nome);
+
+    printf ("Digite sua idade: ");
+    scanf ("%d", &idade);
+    getchar();
+
+    printf ("Digite seu gmail: ");
+    scanf ("%99[^\n]", gmail);
+
+
+    fprintf(arquivo, "%s, %d, %s", nome, idade, gmail);
+
     fclose (arquivo);
+
+    arquivo = fopen("usuarios.txt", "r");
+
+    char Nome2[50], gmail2[100]; int idade2; 
+
+    fscanf (arquivo, "%49[^,],%d,%99[^\n]", nome, &idade, gmail);
+
+    printf ("\nNome: %s \nIdade: %d \ngmail: %s", nome, idade, gmail);
+    fclose(arquivo);
+
+    return 0;
 }
